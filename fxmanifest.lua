96067bee45ae3958694eace7ed346f4e25b69d3e
reyyghi <reyhafizh07@gmail.com>
2023-12-30 15:52:06 +0700

    update manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,21 +5,15 @@ author "Reyghita Hafizh Firmanda"
 
 ui_page "html/ui.html"
 
-shared_scripts {
-	"@ox_lib/init.lua",
-	"@cloudv/modules/utils.lua",
-	"config.lua"
-}
-
 client_scripts {
-	"@cloudv/modules/playerdata.lua",
-	"client.lua",
+	"config.lua",
+	"client/*.lua",
 }
 
 files {
 	"html/ui.html",
 	"html/rm.css",
-	"html/rm.js"
+	"html/rm.js",
 }
 
 lua54 "yes"
